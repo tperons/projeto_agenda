69def3c616dd089d1ba5037e941f9d273f5144f9
tperons <tperons@gmail.com>
2025-03-11 09:42:47 -0300

    settings

--- project/settings.py
+++ project/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://admin:6pNBij8Wrkjz16UEsD0kIMUuCoYNmtbi@dpg-cv81h55ds78s73crm73g-a/contact_db_ltrv',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contact_db_ltrv',
+        'USER': 'admin',
+        'PASSWORD': '6pNBij8Wrkjz16UEsD0kIMUuCoYNmtbi',
+        'HOST': 'dpg-cv81h55ds78s73crm73g-a',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
